914835ad4f4005046af1af7fff56b93b9d186e5e
enkuria <kengeene@gmail.com>
2025-03-09 17:20:44 +0300

    feat: add form to change config details

--- src/components/sip.jsx
+++ src/components/sip.jsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SoftPhone from "react-softphone";
 import { WebSocketInterface } from "jssip";
 
-
-
-
 function App({sip}) {
-  // const ws = new WebSocketInterface("wss://app.sokomoto.co.ke/ws");
-  // https://app.sokomoto.co.ke:8089/ws
-  //  http://13.244.184.44
+  const [domain, setDomain] = useState(import.meta.env.VITE_APP_SIP_DOMAIN);
+  const [userName, setUserName] = useState("100");
+  const [password, setPassword] = useState("123456789");
 
-  const config = {
-    domain: "app.sokomoto.co.ke",
-    uri: "sip:100@app.sokomoto.co.ke", // sip:200@app.sokomoto.co.ke
-    password: "123456789", //  PASSWORD ,
-    ws_servers: "wss://app.sokomoto.co.ke:8089/ws", //ws server
-    sockets: new WebSocketInterface("wss://app.sokomoto.co.ke:8089/ws"),
+  const [config, setConfig] = useState({
+    domain: domain,
+    uri: `sip:${userName}@${domain}`, // sip:200@mydomain.com
+    password: password, //  PASSWORD ,
+    ws_servers: `wss://${domain}:8089/ws`, //wss://${domain}:8089/ws
+    sockets: new WebSocketInterface(`wss://${domain}:8089/ws`), // `wss://${domain}:8089/ws`
     display_name: "***", //jssip Display Name
     debug: true, // Turn debug messages on
-  };
+  });
+
   const setConnectOnStartToLocalStorage = (newValue) => {
     // Handle save the auto connect value to local storage
     return true;
@@ -37,10 +35,38 @@ function App({sip}) {
   };
 
   useEffect(() => {
-    console.log("sip", sip);
+    setConfigDetails();
   }, []);
+
+  function setConfigDetails(){
+    try{
+    setConfig({
+      domain: domain,
+      uri: `sip:${userName}@${domain}`, // sip:200@app.sokomoto.co.ke
+      password: password, //  PASSWORD ,
+      ws_servers: `wss://${domain}:8089/ws`, //ws server
+      sockets: new WebSocketInterface(`wss://${domain}:8089/ws`),
+      display_name: userName, //jssip Display Name
+      debug: true, // Turn debug messages on
+    });
+      alert(`Configured Client Successfully!`);
+    } catch(e){
+      alert(`Error configuring client ${e}`);
+    }
+  }
   return (
     <div className="App">
+      <div className="credentials-form">
+        <label>Username</label>
+        <input value={userName} onChange={(e) => setUserName(e.target.value)} />
+        <label>Password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label>Domain</label>
+        <input value={domain} onChange={(e) => setDomain(e.target.value)} />
+        <button onClick={setConfigDetails} className="credentials-form__button">
+          Set New Credentials
+        </button>
+      </div>
       <header className="App-header">
         <SoftPhone
           callVolume={100} //Set Default callVolume
